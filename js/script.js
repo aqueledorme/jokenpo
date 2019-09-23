91d9792af57f20ab0fe9bcf79716b397da656f3b
silvio <silvio.net.ma@gmail.com>
2019-09-23 14:41:44 -0300

    adicionando função no script

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ pc[2] = "tesoura";
 var texto;
 
 texto = document.getElementById("texto");
+
+function pedra() {
+    aleatorio = Math.floor(Math.random() * pc.length);
+    console.log(pc[aleatorio]);
+
+    if (pc[aleatorio] === pc[0]) {
+        texto.textContent = "empatou";
+    }
+    if (pc[aleatorio] === pc[1]) {
+        texto.textContent = "voce perdeu";
+    }
+    if (pc[aleatorio] === pc[2]) {
+        texto.textContent = "voce ganhou";
+    }
+
+
+}
